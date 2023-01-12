b240ee6b5c4f63db5db7e799d8dbf6a6e1834a4b
Anand Upadhyay <anandgupadhyay03@gmail.com>
2023-01-12 16:03:06 +0530

    Updated logic for one case at a time #1
    Updated logic for one case at a time #1

--- LogicalReasoning/others/AngryProfessor.swift
+++ LogicalReasoning/others/AngryProfessor.swift
@@ -1,12 +1,25 @@
-private func willTheClassBeCancelled(test cases: [[[Int]]]){
-    for test in cases{
-        var studentsOnTime = 0
-        for student in test[1] {
-            if student <= 0 { studentsOnTime += 1}
-        }
-        print(studentsOnTime < test[0][1] ? "YES" : "NO")
+// private func willTheClassBeCancelled(test cases: [[[Int]]]){
+//     for test in cases{
+//         var studentsOnTime = 0
+//         for student in test[1] {
+//             if student <= 0 { studentsOnTime += 1}
+//         }
+//         print(studentsOnTime < test[0][1] ? "YES" : "NO")
+//     }
+// }
+
+// //usage
+// willTheClassBeCancelled(test: testCases)
+
+//For single case at a time
+func angryProfessor(k: Int, a: [Int]) -> String {
+    // Write your code here
+    var studentsOnTime = 0
+    for student in a {
+        if student <= 0 { studentsOnTime += 1}
     }
+    return (studentsOnTime < k) ? "YES" : "NO"
 }
 
-//usage
-willTheClassBeCancelled(test: testCases)
+print(angryProfessor(k: 3, a: [-1,-3,4,2]))//YES
+print(angryProfessor(k: 2, a: [-0,-1,2,1]))//NO
